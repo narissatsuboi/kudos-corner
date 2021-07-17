ff18b723f1a426c3fc81b4ee0d507835f8b0566d
matsagad <matsagad@gmail.com>
2021-07-17 20:23:13 +0800

    Added the base app

---
+++ tif/kudos/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+def dashboard(request):
+	return HttpResponse("<p>dashboard goes here.</p>")
+
+def profile(request):
+	return HttpResponse("<p>user profile goes here.</p>")
