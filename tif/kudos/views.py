88346d655de45fbc82a79ae49c0feefb731589a1
Link <xlinkx_@outlook.com>
2021-07-17 17:08:48 +0100

    profile template connection working and create static/css

--- tif/kudos/views.py
+++ tif/kudos/views.py
@@ -1,7 +1,36 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import User
+from .models import Prize
 
 def dashboard(request):
 	return HttpResponse("<p>dashboard goes here.</p>")
 
 def profile(request):
-	return HttpResponse("<p>user profile goes here.</p>")
+	context = {
+		# "name": request.user.name,
+		# "rank": request.user.ranking,
+		"name": "Link",
+		"rank": "1st",
+		"prize_count": 2,
+		
+	}
+	return render(request, "user/profile.html", context)
+
+
+# to research
+# csrf mechanism for the form
+
+
+
+
+# If a user is logged in (aka authenticated), then you can get their info by
+# accessing variables of (the CustomUser class) request.user
+# so name would be request.user.name and rank would be request.user.ranking
+
+# ok, I see. How will we set it up so that a user is logged in? I.e. how can 
+#  i actually see those values on the page
+
+
+
+
